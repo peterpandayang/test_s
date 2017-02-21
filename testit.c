dbbf221eef2a76475fff48b41123e58dedb06cf3
bingkunyang <bingkunyang@bingkunyangdeMacBook-Pro.local>
2017-02-21 14:17:45 -0800

    a

--- testit.c
+++ testit.c
@@ -1,20 +1,59 @@
 #include <stdio.h>
-
+#include <string.h>
 // int add(int a, int b);
 
+#define VALUE_MAX_STR_LEN 64
+
+struct value_st {
+	int array[VALUE_MAX_STR_LEN];
+}
+
 int ifelse(int a, int b);
+
+// func for sum array
+
+void init_array(int p, int n){
+	int i = 0;
+	for(i = 0; i < n ; i++){
+		p[i] = i;
+		printf("this elem is: %d\n", p[i]);
+	}
+}
+
+int sum_array(int *array, int n){
+    int i;
+    int sum = 0;
+
+    for (i = 0; i < n; i++) {
+        sum = sum + array[i]; 
+    }
+
+    return sum;
+}
+
+int sum_array_s(int *array, int n);
+
+
 
 int main(int argc, char **argv){
     int r;
 
-    // r = add(3,3);
+    struct value_st v_st;
+    init_array(&v_st.array, 10);
 
-    // printf("r = %d\n", r);
+    // int rst = ifelse(3,3);
+    // printf("rst is: %d\n", rst);
 
-    int rst = ifelse(3,3);
-    printf("rst is: %d\n", rst);
+
 
     return 0;
 }
 
 	
+
+
+
+
+
+
+
