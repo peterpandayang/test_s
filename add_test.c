bb2d5965ab7cfc8ad726b8c76420539964e809b2
bingkunyang <bingkunyang@bingkunyangdeMacBook-Pro.local>
2017-02-21 14:04:11 -0800

    a

--- add_test.c
+++ add_test.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-int add(int a, int b);
+// int add(int a, int b);
 
 int ifelse(int a, int b);
 
 int main(int argc, char **argv){
     int r;
 
-    r = add(3,3);
+    // r = add(3,3);
 
-    printf("r = %d\n", r);
+    // printf("r = %d\n", r);
 
     int rst = ifelse(1,3);
     printf("rst is: %d\n", rst);
