5a3e7c02dbf9572b4e1f2c56b8eb92226ced0a12
peterpandayang <byang14@dons.usfca.edu>
2017-02-28 16:21:50 -0800

    a

--- testit.c
+++ testit.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <string.h>
-// int add(int a, int b);
 
 #define VALUE_MAX_STR_LEN 64
 
@@ -9,8 +8,6 @@ struct value_st {
 };
 
 int ifelse(int a, int b);
-
-// func for sum array
 
 void init_array(int *p, int n){
 	int i = 0;
@@ -21,15 +18,45 @@ void init_array(int *p, int n){
     p[i] = '\0';
 }
 
-int sum_array(int *array, int n){
+// func for sum array
+int sum_array_c(int *array, int n){
     int i;
     int sum = 0;
-
     for (i = 0; i < n; i++) {
         sum = sum + array[i]; 
     }
-
     return sum;
+}
+
+int finx_max_c(int *p, int n){
+    int i = 0;
+    int max = array[0];
+    for (i = 1; i < n; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+int fibo_iter_c(int n){
+    int prev = 0, curr = 1, next = 1;
+    for (int i = 0; i < n; i++) {
+        prev = curr;
+        curr = next;
+        next = prev + curr;
+    }
+    return prev;
+}
+
+int fibo_rec_c(int n){
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else {
+        return fib_rec(n - 1) + fib_rec(n - 2);
+    }
 }
 
 
@@ -46,14 +73,16 @@ int main(int argc, char **argv){
     struct value_st v_st;
     init_array(&(v_st.array), 10);
 
-    int sum = sum_array_s(&(v_st.array), 10);
-    printf("sum is: %d\n", sum);
+    int sum_s = sum_array_s(&(v_st.array), 10);
+    printf("sum is: %d\n", sum_s);
 
-    int max = find_max_s(&(v_st.array), 10);
-    printf("max is: %d\n", max);
+    int max_s = find_max_s(&(v_st.array), 10);
+    printf("max is: %d\n", max_s);
 
-    int fibo = fibo_rec_s(5);
-    printf("fibo is %d\n", fibo);
+    int fibo_input = 5;
+
+    int fibo_s = fibo_rec_s(fibo_input);
+    printf("fibo is %d\n", fibo_s);
 
     return 0;
 }
