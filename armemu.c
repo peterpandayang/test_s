b457a96acf7bc078c792ff877b0892858346a507
peterpandayang <byang14@dons.usfca.edu>
2017-03-17 11:45:09 -0700

    a

--- armemu.c
+++ armemu.c
@@ -224,7 +224,7 @@ void armemu_b(struct arm_state *state){
 
     iw = *((unsigned int *) state->regs[PC]);
 
-    printf("prev pc is: %d\n", imme);
+    printf("prev pc is: %d\n", state->regs[PC]);
     if(is_beq_inst(iw)){
         imme = 0xFFFFFF - (iw & 0xFFFFFF) - 1;
         if(state->cpsr == 0x40000000){
@@ -240,8 +240,10 @@ void armemu_b(struct arm_state *state){
         imme = 0xFFFFFF - (iw & 0xFFFFFF) - 1;
         printf("iw is: %u\n", iw);
         printf("immd is: %d\n", imme);
-        state->regs[PC] = state->regs[PC] + 8 + imme * 4;
+        // state->regs[PC] = state->regs[PC] + 8 + imme * 4;
+        state->regs[PC] = state->regs[PC] + 4;
     }
+    printf("later pc is: %d\n", state->regs[PC]);
     state->cpsr = 0;
 }
 
