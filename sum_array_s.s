9ced25aecbe1ce349bb5305e1f0ba502e7e1fe55
peterpandayang <byang14@dons.usfca.edu>
2017-03-17 16:22:44 -0700

    a

--- sum_array_s.s
+++ sum_array_s.s
@@ -2,36 +2,36 @@
 .func sum_array_s
 
 sum_array_s:
-	mov r2, #0
-	mov r3, #0
+	# mov r2, #0
+	# mov r3, #0
 
 
-# 	# add r0, r1, #1
-# 	ldr r4, [r0]
-# 	mov r2, #3
-# 	mov r0, #2
-
-# 	cmp r0, r2
-# 	beq end
-# 	b end2
-
-# end:
-# 	bx lr
-
-# end2:
-# 	mov r0, #3
-# 	bx lr
-
-loop:
-	cmp r2, r1
-	beq done
+	# add r0, r1, #1
 	ldr r4, [r0]
-	add r3, r3, r4
-	add r0, r0, #4
-	add r2, r2, #1
-	b loop
+	mov r2, #3
+	mov r0, #2
 
-done:
-	mov r0, r3
+	cmp r0, r2
+	beq end
+	b end2
+
+end:
 	bx lr
+
+end2:
+	mov r0, #3
+	bx lr
+
+# loop:
+# 	cmp r2, r1
+# 	beq done
+# 	ldr r4, [r0]
+# 	add r3, r3, r4
+# 	add r0, r0, #4
+# 	add r2, r2, #1
+# 	b loop
+
+# done:
+# 	mov r0, r3
+# 	bx lr
 
