a94fc508ecf1ccfd0a28b2bcfdf1ba797f49c14d
peterpandayang <byang14@dons.usfca.edu>
2017-03-17 14:45:42 -0700

    a

--- armemu.c
+++ armemu.c
@@ -171,7 +171,7 @@ void armemu_data_pro(struct arm_state *state){
 /*memory part*/
 bool is_mem_inst(unsigned int iw){
     unsigned int op;
-    op = (iw >> 26) & 0b01;
+    op = (iw >> 26) & 0b11;
     return op == 0b01;
 }
 
