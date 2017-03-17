5394d2226794b45eaea94e9d18622eff084a1c1c
peterpandayang <byang14@dons.usfca.edu>
2017-03-17 11:24:31 -0700

    a

--- armemu.c
+++ armemu.c
@@ -236,7 +236,7 @@ void armemu_b(struct arm_state *state){
     }
     else if(is_b_default_inst(iw)){
         printf("hehehe\n");
-        imme = iw & 0xFFFFFF - 1;
+        imme = iw & 0xFFFFFF;
         printf("immd is: %d\n", imme);
         state->regs[PC] = state->regs[PC] + 8 + imme * 4;
     }
