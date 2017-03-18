e57f172e783d03de33a6194d496b524e7a93b7da
peterpandayang <byang14@dons.usfca.edu>
2017-03-17 17:10:10 -0700

    a

--- armemu.c
+++ armemu.c
@@ -207,7 +207,7 @@ void armemu_ldr(struct arm_state *state){
         i = iw >> 25 & 0b1;
         if(i == 0b0){
             offset = iw & 0xFFF;
-            printf("state->regs[rn] %d\n", state->regs[rn]);
+            printf("state->regs[rn] %d\n", *(unsigned int *)state->regs[rn]);
             state->regs[rd] = *(unsigned int *)state->regs[rn];
         }        
     }
