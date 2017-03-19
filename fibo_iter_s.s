f1063881bba462138b14ea368fb6204dc8d5a56f
peterpandayang <byang14@dons.usfca.edu>
2017-03-19 15:43:04 -0700

    aa

--- fibo_iter_s.s
+++ fibo_iter_s.s
@@ -3,42 +3,42 @@
 
 fibo_iter_s:
 
+	# sub sp, sp, #16
+	# str r0, [sp, #16]
+	# mov r0, #2
+	# mov r1, #3
+	# str r1, [sp, #12]
+	# ldr r0, [sp, #12]
+	# bx lr
+
+
 	sub sp, sp, #16
 	str r0, [sp, #16]
-	mov r0, #2
-	mov r1, #3
-	str r1, [sp, #12]
-	ldr r0, [sp, #12]
+	mov r0, #0
+	str r0, [sp, #12]
+	str r0, [sp]
+	mov r0, #1
+	str r0, [sp, #8]
+	str r0, [sp, #4]
+
+loop:
+	ldr r3, [sp, #12]
+	ldr r12, [sp, #16]
+	cmp r3, r12
+	beq done
+	ldr r0, [sp]
+	ldr r1, [sp, #4]
+	ldr r2, [sp, #8]
+	str r1, [sp]
+	str r2, [sp, #4]
+	add r2, r2, r1
+	str r2, [sp, #8]
+	add r3, r3, #1
+	str r3, [sp, #12]
+	b loop
+
+done:
+	ldr r0, [sp]
 	bx lr
-
-
-# 	sub sp, sp, #16
-# 	str r0, [sp, #16]
-# 	mov r0, #0
-# 	str r0, [sp, #12]
-# 	str r0, [sp]
-# 	mov r0, #1
-# 	str r0, [sp, #8]
-# 	str r0, [sp, #4]
-
-# loop:
-# 	ldr r3, [sp, #12]
-# 	ldr r12, [sp, #16]
-# 	cmp r3, r12
-# 	beq done
-# 	ldr r0, [sp]
-# 	ldr r1, [sp, #4]
-# 	ldr r2, [sp, #8]
-# 	str r1, [sp]
-# 	str r2, [sp, #4]
-# 	add r2, r2, r1
-# 	str r2, [sp, #8]
-# 	add r3, r3, #1
-# 	str r3, [sp, #12]
-# 	b loop
-
-# done:
-# 	ldr r0, [sp]
-# 	bx lr
 
 
