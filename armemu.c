c28e21091974d017f75dc22bac006e04a67c4112
peterpandayang <byang14@dons.usfca.edu>
2017-03-19 16:42:57 -0700

    aa

--- armemu.c
+++ armemu.c
@@ -474,8 +474,8 @@ int main(int argc, char **argv){
 
     sum_array_test(&state, (unsigned int *) sum_array_s, p_pos_array, size);
     find_max_test(&state, (unsigned int *) find_max_s, p_pos_array, size);
-    fibo_iter_test(&state, (unsigned int *) fibo_iter_s, 2);
-    fibo_rec_test(&state, (unsigned int *) fibo_rec_s, 2);
+    fibo_iter_test(&state, (unsigned int *) fibo_iter_s, 3);
+    fibo_rec_test(&state, (unsigned int *) fibo_rec_s, 3);
   
     return 0;
 }
