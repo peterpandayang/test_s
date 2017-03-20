81aee8e2fbe5f31a758bcddf05ea217f1eb701c2
peterpandayang <byang14@dons.usfca.edu>
2017-03-19 21:03:56 -0700

    aa

--- fibo_rec_s.s
+++ fibo_rec_s.s
@@ -2,7 +2,6 @@
 .func fibo_rec_s
 
 fibo_rec_s:
-	mov r5, r0
 	sub sp, sp, #8
 	str lr, [sp]
 
@@ -23,8 +22,6 @@ rec:
 	str r0, [sp, #-4]
 	add sp, sp, #4
 	ldr r0, [sp, #4]
-	mov r0, r0
-
 	sub r0, r0, #2
 	sub sp, sp, #8
 	bl fibo_rec_s
@@ -39,36 +36,3 @@ fibo_s_end:
 	ldr lr, [sp]
 	add sp, sp, #8
 	bx lr
-	
-	
-
-# 	sub sp, sp, #8
-# 	str lr, [sp]
-
-# 	cmp r0, #0
-# 	bne not_0
-# 	b fibo_s_end
-
-# not_0:
-# 	cmp r0, #1
-# 	bne rec
-# 	b fibo_s_end
-
-# rec:
-# 	str r0, [sp, #4]
-# 	sub r0, r0, #1
-# 	bl fibo_rec_s
-# 	str r0, [sp, #-4]
-# 	ldr r0, [sp, #4]
-# 	sub r0, r0, #2
-# 	sub sp, sp, #8
-# 	bl fibo_rec_s
-# 	add sp, sp, #8
-# 	ldr r1, [sp, #-4]
-# 	add r0, r0, r1
-
-
-# fibo_s_end:
-# 	ldr lr, [sp]
-# 	add sp, sp, #8
-# 	bx lr
