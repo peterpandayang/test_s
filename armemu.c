4f272b268652f8614b1850c7bcd3dd16c5f53d02
peterpandayang <byang14@dons.usfca.edu>
2017-03-20 15:41:39 -0700

    aaa

--- armemu.c
+++ armemu.c
@@ -196,7 +196,7 @@ void armemu_mov(struct arm_state *state){
         printf("\n");
         printf("start mov r0 is: %d\n", state->regs[0]);
         printf("start pc is: %d\n", state->regs[15]);
-        printf("stack pointer is: %d\n", state->regs[13] - 8);
+        printf("stack pointer is: %d\n", state->regs[13]);
         rn = iw & 0b1111;
         state->regs[rd] = state->regs[rn];
     }
