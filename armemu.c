30e2a1cbf8e4fee14d3b0ac4e0d38df50bb71eda
peterpandayang <byang14@dons.usfca.edu>
2017-03-19 23:48:09 -0700

    aa

--- armemu.c
+++ armemu.c
@@ -319,11 +319,9 @@ void armemu_bx(struct arm_state *state){
     rn = iw & 0b1111;
     printf("branch and exchange reg is: %d\n", state->regs[rn]); 
     if(state->regs[rn] < 1000){
-        state->regs[PC] = 0;
+        rn = rn + 4;
     } 
-    else{
-        state->regs[PC] = state->regs[rn];
-    }
+    state->regs[PC] = state->regs[rn];
     printf("stack pointer before returning is: %d\n", state->regs[13]);
 }
 
@@ -521,7 +519,7 @@ int main(int argc, char **argv){
     // sum_array_test(&state, (unsigned int *) sum_array_s, p_pos_array, size);
     // find_max_test(&state, (unsigned int *) find_max_s, p_pos_array, size);
     // fibo_iter_test(&state, (unsigned int *) fibo_iter_s, 3);
-    fibo_rec_test(&state, (unsigned int *) fibo_rec_s, 5);
+    fibo_rec_test(&state, (unsigned int *) fibo_rec_s, 4);
   
     return 0;
 }
