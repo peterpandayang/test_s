620b7220845524a2d059d2db2a0d660ac81a2d20
peterpandayang <byang14@dons.usfca.edu>
2017-03-21 14:27:13 -0700

    asd

--- armemu.c
+++ armemu.c
@@ -518,7 +518,9 @@ void find_sub_in_s_test(struct arm_state *as, unsigned int *func, char *p_s, cha
     unsigned int int_p_s = (unsigned int)((unsigned int *)p_s);
     unsigned int int_p_sub = (unsigned int)((unsigned int *)p_sub);
     int s_len = strlen(p_s);
+    int s_sub_len = strlen(p_s_sub);
     printf("s_len is: %d\n", s_len);
+    printf("s_sub_len is: %d\n", s_sub_len);
     printf("inner address is: %d\n", p_s);
     printf("char is: %c\n", *p_s);
     init_arm_state(as, (unsigned int *) func, int_p_s, int_p_sub, 0, 0);
