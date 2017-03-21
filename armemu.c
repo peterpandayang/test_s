2b3a3d1f00a9a53673d10c969ad8a2dc30692377
peterpandayang <byang14@dons.usfca.edu>
2017-03-20 19:44:49 -0700

    aaa

--- armemu.c
+++ armemu.c
@@ -88,10 +88,6 @@ void armemu_add(struct arm_state *state){
     rd = (iw >> 12) & 0xF;
     rn = (iw >> 16) & 0xF;
 
-    if(rd == 13){
-        printf("stack pointer before add is: %d\n", state->regs[rd]);
-    }
-
     if(is_imme_dp(iw)){
         imme = iw & 0xFF;
         state->regs[rd] = state->regs[rn] + imme;
@@ -117,17 +113,7 @@ void armemu_sub(struct arm_state *state){
 
     if(is_imme_dp(iw)){
         imme = iw & 0xFF;
-        if(rd == 13){
-            printf("stack pointer before sub is: %d\n", state->regs[13]);
-            printf("imme is: %d\n", imme);
-            printf("rn is: %d\n", rn);
-        }
         state->regs[rd] = state->regs[rn] - imme;
-        if(rd == 13){
-            printf("stack pointer after sub is: %d\n", state->regs[13]);
-            printf("imme is: %d\n", imme);
-            printf("heihaheiha is hahalala: %d\n", *((unsigned int *) (state->regs[SP] + 8)));
-        }
     }
     else{
         rm = iw & 0xF;
@@ -194,19 +180,8 @@ void armemu_mov(struct arm_state *state){
         state->regs[rd] = imme;
     }
     else{
-        printf("\n");
-        printf("start mov r0 is: %d\n", state->regs[0]);
-        printf("start pc is: %d\n", state->regs[15]);
-        printf("stack pointer is: %d\n", state->regs[13]);
         rn = iw & 0b1111;
         state->regs[rd] = state->regs[rn];
-        if(rn == 0 && state->regs[rn] == 3){
-            printf("heihaheiha is hahalala: %d\n", *((unsigned int *) state->regs[SP]));
-        }
-        if(rn == 0 && state->regs[rn] == 2){
-            printf("heihaheiha is hahalala: %d\n", *((unsigned int *) (state->regs[SP] + 8)));
-        }
-
     }
 
     if (rd != PC) {
@@ -273,9 +248,7 @@ void armemu_ldr(struct arm_state *state){
         i = iw >> 25 & 0b1;
         if(i == 0b0){
             offset = iw & 0xFFF;
-            printf("offset is: %d\n", offset);
             state->regs[rd] = *((unsigned int *)(state->regs[rn] + offset));
-            printf("we are loading: %d\n", *((unsigned int *)state->regs[rn] + offset));
         }        
     }
 
@@ -296,10 +269,6 @@ void armemu_str(struct arm_state *state){
         if(i == 0b0){
             offset = iw & 0xFFF;
             *((unsigned int *)(state->regs[rn] + offset)) = state->regs[rd];
-            printf("we are storing: %d\n", state->regs[rd]);
-            if(state->regs[0] == 3){
-                printf("heihaheiha is hahalala: %d\n", *((unsigned int *) state->regs[SP] + 8));
-            }
         }        
     }
 
@@ -343,9 +312,7 @@ void armemu_bx(struct arm_state *state){
 
     iw = *((unsigned int *) state->regs[PC]);
     rn = iw & 0b1111;
-    printf("branch and exchange reg is: %d\n", state->regs[rn]); 
     state->regs[PC] = state->regs[rn];
-    printf("stack pointer before returning is: %d\n", state->regs[13]);
 }
 
 bool is_b_inst(unsigned int iw){
@@ -407,11 +374,7 @@ void armemu_b(struct arm_state *state){
     }
 
     if(is_bl_inst(iw)){
-        printf("branch and link's r0 is: %d\n", state->regs[0]);
-        printf("link regiter before str reg is: %d\n", state->regs[LR]);
         save_link_addr(state);
-        printf("link regiter after is: %d\n", state->regs[LR]);
-        printf("program counter before link and branch is: %d\n", state->regs[PC]);
         state->regs[PC] = state->regs[PC] + 8 + offset * 4;
     }
     else if(is_beq_inst(iw)){
@@ -506,29 +469,6 @@ void fibo_rec_test(struct arm_state *as, unsigned int *func, int size){
 
 
 
-int fibo_test(int size){
-    int i;
-    int iter_s;
-    int iter_c;
-    int rec_s;
-    int rec_c;
-
-
-    printf("Test for fibonacci sequence of recursion with size %d: \n", size);
-    printf("(Assembly)\n");
-
-    for(i = 0; i < size; i++){
-        int rec_c = fibo_rec_s(i);
-        printf("%d ", rec_c);
-    }
-    printf("(C)\n");
-    printf("\n");
-    return 0;
-}
-
-
-
-
 /*main part*/
 int main(int argc, char **argv){
     struct arm_state state;
@@ -538,7 +478,6 @@ int main(int argc, char **argv){
 
     init_array_c(p_pos_array, size);
 
-    fibo_test(size);
     // sum_array_test(&state, (unsigned int *) sum_array_s, p_pos_array, size);
     // find_max_test(&state, (unsigned int *) find_max_s, p_pos_array, size);
     // fibo_iter_test(&state, (unsigned int *) fibo_iter_s, 3);
