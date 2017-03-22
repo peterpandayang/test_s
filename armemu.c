71ceb65ff2ba0d7efe9da00773db67495e5fa356
peterpandayang <byang14@dons.usfca.edu>
2017-03-22 00:07:08 -0700

    asd

--- armemu.c
+++ armemu.c
@@ -469,6 +469,14 @@ unsigned int armemu(struct arm_state *state){
 
     while (state->regs[PC] != 0) {
         armemu_one(state);
+        printf("r0 is: %d\n", state->regs[0]);
+        printf("r1 is: %d\n", state->regs[1]);
+        printf("r2 is: %d\n", state->regs[2]);
+        printf("r3 is: %c\n", state->regs[3]);
+        printf("r3 is: %d\n", state->regs[3]);
+        printf("r12 is: %c\n", state->regs[12]);
+        printf("PC is: %d\n", state->regs[PC]);
+        printf("\n");
     }
 
     return state->regs[0];
