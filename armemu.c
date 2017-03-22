66af7c16f829cb27b61892bfbac99e6aeb50360b
peterpandayang <byang14@dons.usfca.edu>
2017-03-21 23:59:21 -0700

    asd

--- armemu.c
+++ armemu.c
@@ -551,7 +551,6 @@ int main(int argc, char **argv){
     find_max_test(&state, (unsigned int *) find_max_s, p_pos_array, size);
     fibo_iter_test(&state, (unsigned int *) fibo_iter_s, size);
     fibo_rec_test(&state, (unsigned int *) fibo_rec_s, size);
-    printf("this is alright\n");
     find_sub_in_s_test(&state, (unsigned int *) find_sub_in_s_s, p_s, p_sub);
   
     return 0;
