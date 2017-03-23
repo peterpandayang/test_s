e0cd6b245829816187fd136bf3190c6f0bc26de8
peterpandayang <byang14@dons.usfca.edu>
2017-03-23 14:05:48 -0700

    asdf

--- armemu.c
+++ armemu.c
@@ -469,10 +469,10 @@ unsigned int armemu(struct arm_state *state){
     return state->regs[0];
 }
 
-void print_array_c(int *p_array, int n){
+void print_array_c(int p_array, int n){
     int i = 0;
     for(i = 0; i < n ; i++){
-        printf("%d ", p_array[i]);
+        printf("%d ", *((unsigned int *)p_array[i]));
     }
     printf("\n");
 }
@@ -480,7 +480,7 @@ void print_array_c(int *p_array, int n){
 /*test part*/
 void sum_array_test(struct arm_state *as, unsigned int *func, int *p_array, int size){
     printf("Start sum array test and print input array......\n");
-    print_array_c((int)p_array, size);
+    print_array_c((unsigned int)p_array, size);
     init_arm_state(as, (unsigned int *) func, (unsigned int) p_array, size, 0, 0);
     int sum;
     sum = armemu(as);
