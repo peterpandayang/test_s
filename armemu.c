c77f80969e5f3d25f7b7277c74c5ccff5752d6f3
peterpandayang <byang14@dons.usfca.edu>
2017-03-24 00:34:37 -0700

    print time

--- armemu.c
+++ armemu.c
@@ -656,6 +656,16 @@ void write_to_output(struct arm_state *state, int index){
     /* print some text */
     const char *text = "Write this to the file";
     fprintf(f, "Some text: %s\n", text);
+    /* print integers and floats */
+    int i = 1;
+    float py = 3.1415927;
+    fprintf(f, "Integer: %d, float: %f\n", i, py);
+
+    /* printing single chatacters */
+    char c = 'A';
+    fprintf(f, "A character: %c\n", c);
+
+    fclose(f);
 }
 
 void find_sub_in_s_test(struct arm_state *state, unsigned int *func, char *p_s, char *p_sub){
