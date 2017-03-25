5bd58083d214a2f027850006dfae68ecf0f2e197
peterpandayang <byang14@dons.usfca.edu>
2017-03-25 10:42:50 -0700

    asdf

--- armemu.c
+++ armemu.c
@@ -600,8 +600,8 @@ void gettime_array(struct arm_state *state, unsigned int *func, int *p_array, in
     int i;
     long total_nsecs = 0;
     time_t total_secs = 0;
-    double total_time_secs = 0.0;
-    double total_time_usecs = 0.0;
+    double armemu_total_time_secs = 0.0;
+    double armemu_total_time_usecs = 0.0;
     // armemu time
     clock_gettime(CLOCK_MONOTONIC, &t1);
     for (i = 0; i < ITERS_ARRAY; i++) {
@@ -611,15 +611,15 @@ void gettime_array(struct arm_state *state, unsigned int *func, int *p_array, in
     clock_gettime(CLOCK_MONOTONIC, &t2); 
     total_secs = t2.tv_sec - t1.tv_sec;
     total_nsecs = t2.tv_nsec - t1.tv_nsec;
-    total_time_secs = (double) total_secs + ((double) total_nsecs) / 1000000000.0;
-    total_time_usecs = (((double) total_nsecs) / ((double) ITERS_ARRAY)) * 1000000.0;
-    state->armemu_total_time_usecs = total_time_secs;
-    state->armemu_total_time_secs = total_time_usecs;
+    armemu_total_time_secs = (double) total_secs + ((double) total_nsecs) / 1000000000.0;
+    armemu_total_time_usecs = (((double) total_nsecs) / ((double) ITERS_ARRAY)) * 1000000.0;
+    state->armemu_total_time_usecs = armemu_total_time_secs;
+    state->armemu_total_time_secs = armemu_total_time_usecs;
     //native time
     total_nsecs = 0;
     total_secs = 0;
-    total_time_secs = 0.0;
-    total_time_usecs = 0.0;
+    double total_time_secs = 0.0;
+    double total_time_usecs = 0.0;
     clock_gettime(CLOCK_MONOTONIC, &t1);
     if(index == 1){
         for (i = 0; i < ITERS_ASSEM; i++) {
