937640a91da304ffa266ca7a0a7a8c45e8738898
peterpandayang <byang14@dons.usfca.edu>
2017-03-27 15:26:45 -0700

    asdf

--- armemu.c
+++ armemu.c
@@ -844,15 +844,13 @@ void fibo_rec_test(struct arm_state *state, unsigned int *func, int size){
     init_arm_state(state, (unsigned int *) func, 10, 0, 0, 0);
     int fibo_rec;
     fibo_rec = armemu(state);
-    printf("Fibo recursion result for %d's element is: %d\n", 10, fibo_rec);
-    // printf("Fibo recursion result for %d's element is: %d\n", 10, fibo_rec);
+    printf("Fibo recursion result for %d's element is:\n", 10);
     gettime_fibo(state, (unsigned int *) func, 10, 2);
     print_analysis(state);
     write_to_output(state, 11);
     init_arm_state(state, (unsigned int *) func, 19, 0, 0, 0);
     fibo_rec = armemu(state);
-    // printf("Fibo recursion result for %d's element is: %d\n", 19, fibo_rec);
-    printf("Fibo recursion result for %d's element is: %d\n", 19, fibo_rec);
+    printf("Fibo recursion result for %d's element is:\n", 19);
     gettime_fibo(state, (unsigned int *) func, 19, 2);
     print_analysis(state);
     write_to_output(state, 20);
