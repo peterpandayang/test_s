79ae3134462dbdb5cc3053a039db4d45e5c8839b
peterpandayang <byang14@dons.usfca.edu>
2017-03-27 18:33:49 -0700

    asdf

--- armemu.c
+++ armemu.c
@@ -4,7 +4,6 @@
 #include <sys/times.h>
 #include <time.h>
 #include <unistd.h>
-#include "write_to_output.h"
 
 #define NREGS 16
 #define STACK_SIZE 1024
@@ -649,6 +648,7 @@ void print_analysis(struct arm_state *state){
     printf("native method is: %d times faster than armemu method\n\n", times); 
 }
 
+/*gettime part*/
 void gettime_array(struct arm_state *state, unsigned int *func, int *p_array, int size, int index){
     struct timespec t1, t2;
     int i;
@@ -759,6 +759,7 @@ void gettime_find_s_in_sub(struct arm_state *state, unsigned int *func, int p_s,
     state->native_total_time_usecs = total_time_usecs;
 }
 
+/*write part*/
 void write_inst_percentage(FILE *f, struct arm_state *state){
     fprintf(f, "\nTable for percentage of instructions\n");
     fprintf(f, "\nComputation      Memory       Branches\n");
@@ -776,89 +777,89 @@ void write_inst_percentage(FILE *f, struct arm_state *state){
     fprintf(f, "        %f%%\n", branch_not_taken_perc);
 }
 
-// void write_to_output(struct arm_state *state, int index){
-//     FILE *f;
-//     const char *title;
-//     if(index == 1){
-//         f = fopen("sum_array_pos.txt", "w");
-//         title = "Result for sum positive array test:";
-//         fprintf(f, "%s\n", title);
-//     }
-//     else if(index == 2){
-//         f = fopen("sum_array_neg.txt", "w");
-//         title = "Result for sum negetive array test:";
-//         fprintf(f, "%s\n", title);
-//     }
-//     else if(index == 3){
-//         f = fopen("sum_array_zero.txt", "w");
-//         title = "Result for sum zero array test:";
-//         fprintf(f, "%s\n", title);
-//     }
-//     else if(index == 4){
-//         f = fopen("sum_array_large.txt", "w");
-//         title = "Result for large array test:";
-//         fprintf(f, "%s\n", title);
-//     }
-//     else if(index == 5){
-//         f = fopen("find_max_pos.txt", "w");
-//         title = "Result for find max test:";
-//         fprintf(f, "%s\n", title);
-//     }
-//     else if(index == 6){
-//         f = fopen("find_max_neg.txt", "w");
-//         title = "Result for find max test:";
-//         fprintf(f, "%s\n", title);
-//     }
-//     else if(index == 7){
-//         f = fopen("find_max_zero.txt", "w");
-//         title = "Result for find max test:";
-//         fprintf(f, "%s\n", title);
-//     }
-//     else if(index == 8){
-//         f = fopen("find_max_large.txt", "w");
-//         title = "Result for find max test:";
-//         fprintf(f, "%s\n", title);
-//     }
-//     else if(index == 10){
-//         f = fopen("fibo_iter_ten.txt", "w");
-//         title = "Result for fibo iteration test of 10:";
-//         fprintf(f, "%s\n", title);
-//     }
-//     else if(index == 19){
-//         f = fopen("fibo_iter_nineteen.txt", "w");
-//         title = "Result for fibo iteration test of 19:";
-//         fprintf(f, "%s\n", title);
-//     }
-//     else if(index == 11){
-//         f = fopen("fibo_rec_ten.txt", "w");
-//         title = "Result for fibo recursion test of 10:";
-//         fprintf(f, "%s\n", title);
-//     }
-//     else if(index == 20){
-//         f = fopen("fibo_rec_nineteen.txt", "w");
-//         title = "Result for fibo recursion test of 19:";
-//         fprintf(f, "%s\n", title);
-//     }
-//     else if(index == 9){
-//         f = fopen("find_sub_in_s.txt", "w");
-//         title = "Result for find sub in s test:";
-//         fprintf(f, "%s\n", title);
-//     }
-//     fprintf(f, "Total instructions: %d\n", (char *)(state->exec_instr_count));
-//     fprintf(f, "Total computation instructions: %d\n", (char *)(state->compu_count));
-//     fprintf(f, "Total memory instructions: %d\n", (char *)(state->mem_count));
-//     fprintf(f, "Total branch instructions: %d\n", (char *)(state->branch_count));
-//     fprintf(f, "Total branch taken: %d\n", (char *)(state->b_taken_count));
-//     fprintf(f, "Total branch not taken: %d\n", (char *)(state->b_not_taken_count));
-//     write_read_regs_to_file(state, f);
-//     write_written_regs_to_file(state, f);
-//     fprintf(f, "armemu_total_time_usecs = %f us\n", state->armemu_total_time_usecs);  
-//     fprintf(f, "native_total_time_usecs = %f us\n", state->native_total_time_usecs);
-//     int times = state->armemu_total_time_usecs / state->native_total_time_usecs;
-//     fprintf(f, "native method is: %d times faster than armemu method\n", times);
-//     write_inst_percentage(f, state);
-//     fclose(f);
-// }
+void write_to_output(struct arm_state *state, int index){
+    FILE *f;
+    const char *title;
+    if(index == 1){
+        f = fopen("sum_array_pos.txt", "w");
+        title = "Result for sum positive array test:";
+        fprintf(f, "%s\n", title);
+    }
+    else if(index == 2){
+        f = fopen("sum_array_neg.txt", "w");
+        title = "Result for sum negetive array test:";
+        fprintf(f, "%s\n", title);
+    }
+    else if(index == 3){
+        f = fopen("sum_array_zero.txt", "w");
+        title = "Result for sum zero array test:";
+        fprintf(f, "%s\n", title);
+    }
+    else if(index == 4){
+        f = fopen("sum_array_large.txt", "w");
+        title = "Result for large array test:";
+        fprintf(f, "%s\n", title);
+    }
+    else if(index == 5){
+        f = fopen("find_max_pos.txt", "w");
+        title = "Result for find max test:";
+        fprintf(f, "%s\n", title);
+    }
+    else if(index == 6){
+        f = fopen("find_max_neg.txt", "w");
+        title = "Result for find max test:";
+        fprintf(f, "%s\n", title);
+    }
+    else if(index == 7){
+        f = fopen("find_max_zero.txt", "w");
+        title = "Result for find max test:";
+        fprintf(f, "%s\n", title);
+    }
+    else if(index == 8){
+        f = fopen("find_max_large.txt", "w");
+        title = "Result for find max test:";
+        fprintf(f, "%s\n", title);
+    }
+    else if(index == 10){
+        f = fopen("fibo_iter_ten.txt", "w");
+        title = "Result for fibo iteration test of 10:";
+        fprintf(f, "%s\n", title);
+    }
+    else if(index == 19){
+        f = fopen("fibo_iter_nineteen.txt", "w");
+        title = "Result for fibo iteration test of 19:";
+        fprintf(f, "%s\n", title);
+    }
+    else if(index == 11){
+        f = fopen("fibo_rec_ten.txt", "w");
+        title = "Result for fibo recursion test of 10:";
+        fprintf(f, "%s\n", title);
+    }
+    else if(index == 20){
+        f = fopen("fibo_rec_nineteen.txt", "w");
+        title = "Result for fibo recursion test of 19:";
+        fprintf(f, "%s\n", title);
+    }
+    else if(index == 9){
+        f = fopen("find_sub_in_s.txt", "w");
+        title = "Result for find sub in s test:";
+        fprintf(f, "%s\n", title);
+    }
+    fprintf(f, "Total instructions: %d\n", (char *)(state->exec_instr_count));
+    fprintf(f, "Total computation instructions: %d\n", (char *)(state->compu_count));
+    fprintf(f, "Total memory instructions: %d\n", (char *)(state->mem_count));
+    fprintf(f, "Total branch instructions: %d\n", (char *)(state->branch_count));
+    fprintf(f, "Total branch taken: %d\n", (char *)(state->b_taken_count));
+    fprintf(f, "Total branch not taken: %d\n", (char *)(state->b_not_taken_count));
+    write_read_regs_to_file(state, f);
+    write_written_regs_to_file(state, f);
+    fprintf(f, "armemu_total_time_usecs = %f us\n", state->armemu_total_time_usecs);  
+    fprintf(f, "native_total_time_usecs = %f us\n", state->native_total_time_usecs);
+    int times = state->armemu_total_time_usecs / state->native_total_time_usecs;
+    fprintf(f, "native method is: %d times faster than armemu method\n", times);
+    write_inst_percentage(f, state);
+    fclose(f);
+}
 
 /*test part*/
 void single_sum_array_test(struct arm_state *state, unsigned int *func, int *p_array, int size, int index){
