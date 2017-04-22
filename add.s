85bfdfbb4551426d121600454141b3a96b41fa28
peterpandayang <byang14@dons.usfca.edu>
2017-04-22 14:56:52 -0700

    asdf

--- add.s
+++ add.s
@@ -2,7 +2,20 @@
 .func add
 
 add:
-	mov r0, #1
-	add r0, r0, #1
+	mov r2, #0
+	mov r3, #0
+	
+loop:
+	cmp r2, r1
+	beq done
+	ldr r12, [r0]
+	add r3, r3, r12
+	add r0, r0, #4
+	add r2, r2, #1
+	b loop
+
+done:
+	mov r0, r3
+	bx lr
 	
 
